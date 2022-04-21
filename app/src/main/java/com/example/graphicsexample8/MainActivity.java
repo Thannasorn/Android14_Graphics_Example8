79d55c69df684837c8df98a15e05c9fa046d70aa
Thannasorn Metta <vk0831671865@gmail.com>
2022-04-21 16:49:32 +0700

    Initial commit

---
+++ app/src/main/java/com/example/graphicsexample8/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.graphicsexample8;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(new GraphicsView(this));
+    }
+}
